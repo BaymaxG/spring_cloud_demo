a4f8bdf22aadf7ac3bf0d90609f531849690cacc
Baymax <1592331270@qq.com>
2020-05-10 11:31:31 +0800

    [desc]抽取BaseEntity [expand]NA

--- product_service/src/main/java/cn/itcast/product/entity/Product.java
+++ product_service/src/main/java/cn/itcast/product/entity/Product.java
@@ -1,49 +1,22 @@
 package cn.itcast.product.entity;
 
 import cn.itcast.product.entity.listener.ProductListener;
+import com.baymax.entity.BaseEntity;
 import lombok.Data;
-import org.hibernate.annotations.GenericGenerator;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.EntityListeners;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
 import javax.persistence.Table;
-import java.util.Date;
 
 /**
  * 商品实体类
  */
 @Data
 @Entity
-@Table(name = "db_product_t")
+@Table(name = "dev_product_t")
 @EntityListeners(value = ProductListener.class)
-public class Product {
-    @Id
-    @GeneratedValue(generator="sys_uid")
-    @GenericGenerator(name="sys_uid", strategy="uuid")
-    @Column(name = "id", columnDefinition = "VARCHAR(255) COMMENT '主键'")
-    private String id;
-
-    @Column(name = "create_by", updatable = false, columnDefinition = "VARCHAR(200) COMMENT '创建人'")
-    private String createBy;
-
-    @Column(name = "create_account", updatable = false, columnDefinition = "BIGINT(20)  COMMENT '创建人账号'")
-    private long createAccount;
-
-    @Column(name = "create_date", updatable = false, columnDefinition = "DATETIME COMMENT '创建时间'")
-    private Date createDate;
-
-    @Column(name = "last_update_by", columnDefinition = "VARCHAR(200) COMMENT '最后更新人'")
-    private String lastUpdateBy;
-
-    @Column(name = "last_update_account", columnDefinition = "BIGINT(20) COMMENT '最后更新人账号'")
-    private long lastUpdateAccount;
-
-    @Column(name = "last_update_date", columnDefinition = "DATETIME COMMENT '最后更新时间'")
-    private Date lastUpdateDate;
-
+public class Product extends BaseEntity {
     @Column(name = "product_name", columnDefinition = "VARCHAR(40) COMMENT '产品名称'")
     private String productName;
 
--- product_service/src/main/java/cn/itcast/product/entity/listener/ProductListener.java
+++ product_service/src/main/java/cn/itcast/product/entity/listener/ProductListener.java
@@ -12,9 +12,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.persistence.PostLoad;
-import javax.persistence.PrePersist;
-import javax.persistence.PreUpdate;
-import java.util.Date;
 
 /**
  * @author guNingbo
@@ -22,31 +19,6 @@ import java.util.Date;
  */
 public class ProductListener {
     private static final Logger LOGGER = LoggerFactory.getLogger(ProductListener.class);
-
-    /**
-     * 被 @PrePersist- 在新实体持久化之前（添加到EntityManager）
-     * 被 @PostPersist- 在数据库中存储新实体（在commit或期间flush）
-     * 被 @PostLoad - 从数据库中检索实体后。
-     * 被 @PreUpdate- 当一个实体被识别为被修改时EntityManager
-     * 被 @PostUpdate- 更新数据库中的实体（在commit或期间flush）
-     * 被 @PreRemove - 在EntityManager中标记要删除的实体时
-     */
-    @PrePersist
-    public void prePersist(Product product) {
-        product.setCreateAccount(12345678L);
-        product.setCreateBy("test");
-        product.setCreateDate(new Date());
-        product.setLastUpdateBy("test-first");
-        product.setLastUpdateAccount(12345678L);
-        product.setLastUpdateDate(new Date());
-    }
-
-    @PreUpdate
-    public void preUpdate(Product product) {
-        product.setLastUpdateBy("test-after");
-        product.setLastUpdateAccount(87654321L);
-        product.setLastUpdateDate(new Date());
-    }
 
     @PostLoad
     public void loadItem(Product product) {
